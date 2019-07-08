d1e06edbb99340d7b056dc22a3190ff66b8cb113
Jia Xu <jia@unity3d.com>
2019-07-08 11:56:35 -0700

    - Update & Merge from Develop

--- Assets/QA/Input_Test/Scripts/Input/PenISX.cs
+++ Assets/QA/Input_Test/Scripts/Input/PenISX.cs
@@ -86,7 +86,17 @@ public class PenISX : MonoBehaviour
                            + pen.position.ReadValue().ToString("F0") + "\n"
                            + pen.tilt.ReadValue().ToString("F2") + "\n"
                            + pen.twist.ReadValue().ToString("F2") + "\n"
-                           + pen.delta.ReadValue().ToString("F2");        
+                           + pen.delta.ReadValue().ToString("F2");
+
+        // Update pressure indicator
+        float pressure = pen.pressure.ReadValue();
+        Color newColor = Color.red;
+        newColor.a = pressure;
+        m_pressureText.color = newColor;
+        m_pressureText.text = "Pressure: " + pressure.ToString("F2");
+
+        // Update inRange state/indicator
+        m_outOfRangeSign.SetActive(!pen.inRange.isPressed);
     }
 
     private void ButtonPress(ButtonControl control)
